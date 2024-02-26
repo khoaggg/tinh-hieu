45d40ebd0661872ff38fd7e11abdc92fe5900f71
ADMIN <ADMIN@LAB1-MAY23>
2024-02-26 08:27:30 +0700

    Add project files.

---
+++ tinh hieu/tinh hieu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int so1, so2, hieu;
+    cout << "CHUONG TRINH TINH HIEU\N";
+    cout << "nhap so thu nhat:";
+    cin >> so1;
+    cout << "nhap so thu hai:";
+    cin >> so2;
+    hieu = so1 - so2;
+    cout << so1 << "-" << so2 << "="
+        << hieu
+        << endl;
+    return 0;
+}
